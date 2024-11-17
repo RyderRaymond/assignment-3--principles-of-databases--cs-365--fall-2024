e7e00336b4e6a1c3e1a8b3627c1e2cd7835a5d28
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2024-11-16 19:43:06 -0500

    Added user to config.php

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Contains the define mappings for the database credentials listed in the file
+ * “setup.sql,” which is in the “mysql” folder in the root of this project
+ * directory.
+ */
+
+define("DBNAME", "student_passwords");
+define("DBHOST", "localhost");
+define("DBUSER", "passwords_user");
+define("DBPASS", "HVGyt789uIOJknbhvgytf&^89uionjk");
+
+$base_url = dirname(__FILE__);
